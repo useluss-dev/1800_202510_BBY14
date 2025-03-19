cda0db3d5afbb1b9476bc7ca401c6d708aa088f9
sammuu <47285750+samuelpita@users.noreply.github.com>
2025-03-19 10:21:26 -0700

    Reworked review page

--- src/review.js
+++ src/review.js
@@ -1,16 +1,34 @@
+import firebase from "firebase";
+import { app, db } from "./firebaseAPI_BBY14";
+
+function getStarInputValue(purpose) {
+    const radioButtons = document.querySelectorAll(`input[name='${purpose}]`);
+}
+
+function submitReview() {
+    //
+}
+
+function getLandlordDocSnapshot() {
+    const landlordID = new URLSearchParams(window.location.search).get("landlord-id");
+    const landlordCollection = db.collection("landlords");
+
+    return landlordCollection.doc(landlordID).get();
+}
+
 function applyStarInputs(purpose) {
-    const radioButtons = document.querySelectorAll(`input[name='rating-${purpose}']`);
+    const radioButtons = document.querySelectorAll(`input[name='${purpose}']`);
     radioButtons.forEach((radio) => {
         radio.addEventListener("change", () => {
-            const [purpose, index] = radio.id.split("-").slice(1);
+            const [purpose, index] = radio.id.split("-");
 
             for (let i = 1; i <= radioButtons.length; i++) {
-                let label = document.querySelector(`label[for='rating-${purpose}-${i}']`);
+                let label = document.querySelector(`label[for='${purpose}-${i}']`);
                 label.childNodes[0].src = "/assets/StarIcon.svg";
             }
 
             for (let i = 1; i <= radio.value; i++) {
-                let label = document.querySelector(`label[for='rating-${purpose}-${i}']`);
+                let label = document.querySelector(`label[for='${purpose}-${i}']`);
                 label.childNodes[0].src = "/assets/StarIconActive.svg";
             }
         });
@@ -18,13 +36,19 @@ function applyStarInputs(purpose) {
 }
 
 function setupStarInputs(purpose) {
-    let sectionField = document.getElementById(`rating-${purpose}`);
+    let sectionField = document.getElementById(purpose);
     let heading =
         purpose == "overall" ? document.createElement("h3") : document.createElement("h5");
     let starDiv = document.createElement("div");
 
-    heading.className = "text-gray-500 uppercase mb-2";
+    heading.className = "text-gray-500 uppercase mb-1";
     heading.textContent = purpose;
+
+    heading.textContent += " ";
+    let span = heading.appendChild(document.createElement("span"));
+    span.className = "text-red-500";
+    span.textContent = "*";
+
     starDiv.className = "w-full flex gap-2";
 
     for (let i = 1; i <= 5; i++) {
@@ -33,8 +57,8 @@ function setupStarInputs(purpose) {
         let starImg = starLabel.appendChild(document.createElement("img"));
 
         star.type = "radio";
-        star.id = `rating-${purpose}-${i}`;
-        star.name = `rating-${purpose}`;
+        star.id = `${purpose}-${i}`;
+        star.name = `${purpose}`;
         star.value = i;
         star.className = "hidden";
         starImg.src = "/assets/StarIcon.svg";
@@ -48,8 +72,20 @@ function setupStarInputs(purpose) {
     applyStarInputs(purpose);
 }
 
-setupStarInputs("overall");
-setupStarInputs("behavior");
-setupStarInputs("rules");
-setupStarInputs("rent");
-setupStarInputs("amenities");
+getLandlordDocSnapshot()
+    .then((docSnapshot) => {
+        const data = docSnapshot.data();
+
+        document.getElementById("landlord-null").className = "hidden";
+        document.getElementById("landlord-name").textContent = data.name;
+
+        setupStarInputs("overall");
+        setupStarInputs("behavior");
+        setupStarInputs("rules");
+        setupStarInputs("quality");
+        setupStarInputs("rent");
+        setupStarInputs("amenities");
+    })
+    .catch((error) => {
+        document.getElementById("landlord-exists").className = "hidden";
+    });
